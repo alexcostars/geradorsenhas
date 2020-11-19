eb795a5b87eb8f6c3c63b517e39e510ee87c9fb9
Alex Costa <alexcostars@gmail.com>
2020-11-19 18:57:57 -0300

    commit inicial

---
+++ src/main/java/com/alexcosta/geradorsenhas/GeradorSenhasApplication.java
@@ -0,0 +1,13 @@
+package com.alexcosta.geradorsenhas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeradorSenhasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeradorSenhasApplication.class, args);
+	}
+
+}
